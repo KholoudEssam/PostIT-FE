66b9608f1999e6d90c2bc923597cdee69a54fc63
Kholoud Essam <kholoud.essamfciscu@gmail.com>
2021-05-05 03:36:18 +0200

    fix issue in postsModule

--- src/app/components/posts/posts.module.ts
+++ src/app/components/posts/posts.module.ts
@@ -4,9 +4,15 @@ import { PostCreateComponent } from './post-create/post-create.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppAngularMaterialModule } from 'src/app/app-angular-material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PostCreateComponent, PostListComponent],
-  imports: [CommonModule, ReactiveFormsModule, AppAngularMaterialModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AppAngularMaterialModule,
+    RouterModule,
+  ],
 })
 export class PostsModule {}
